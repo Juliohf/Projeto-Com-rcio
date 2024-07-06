f40e7c3d6b5d7bf57a5052a2f0efa9a5ffe7aaf9
Julio <juliohcf@hotmail.com>
2024-07-06 15:51:58 -0300

    Equals and Hashcode

--- src/main/java/com/generation/dscommerce/entities/Order.java
+++ src/main/java/com/generation/dscommerce/entities/Order.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 import java.time.Instant;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 import java.util.List;
 
@@ -89,4 +90,17 @@ public class Order {
         return items.stream().map(x -> x.getProduct()).toList();
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Order order = (Order) o;
+        return Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
